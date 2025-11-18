fb6f239e756c31744de79fecaec090932193fec4
Dangol <monika.dangol@capgemini.com>
2025-11-18 15:42:18 +0000

    edited the underwriter page to view the list of insureds

--- insurance-AI/src/App.tsx
+++ insurance-AI/src/App.tsx
@@ -1,9 +1,11 @@
 import UserLogin from "./components/userLogin";
 import AdminPage from "./components/AdminPage";
+import UnderwriterPage from "./components/UnderwriterPage";
 
 function App() {
   return (
     <>
+      <UnderwriterPage />
       <UserLogin />
       {/* Render AdminPage - it fetches users from http://localhost:3000/users */}
       <AdminPage />
