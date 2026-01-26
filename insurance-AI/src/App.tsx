c54e48d93834d9f119221440995af84345cb9ad6
Monika <dangolmonika@yahoo.com>
2026-01-26 17:33:09 +0000

    fixed issue of duplicate chatbot UI

--- insurance-AI/src/App.tsx
+++ insurance-AI/src/App.tsx
@@ -1,30 +1,22 @@
-import { useEffect, useState } from "react";
+
 import UserLogin from "./components/userLogin";
-import AdminPage from "./components/AdminPage";
+
 import UnderwriterPage from "./components/UnderwriterPage";
-<<<<<<< HEAD
 import Chatbot from "./components/Chatbot";
-=======
-import ChatWidget from "./components/ChatWidget";
 
 
 
->>>>>>> ab546b7282011979dd8f752fde2b263cca70168b
+
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Chatbot />
+    
       <UnderwriterPage />
-      <UserLogin />
-=======
-      {/* <UnderwriterPage /> */}
+     
       <UserLogin /> 
->>>>>>> ab546b7282011979dd8f752fde2b263cca70168b
-      {/* Render AdminPage - it fetches users from http://localhost:3000/users */}
-      {/* <AdminPage /> */}
-      <ChatWidget />
+    
+      <Chatbot />
       
     </>
   );
